06747571823b2ac192e5e86257ad2bfe0bc093ec
Fabian Reinders <fabian@fabiancdng.com>
2023-08-08 13:00:42 +0200

    ♻️Refactor register- and render- calls (less methods, more abstraction).

--- src/Options.php
+++ src/Options.php
@@ -35,7 +35,7 @@ class Options {
 		register_setting( 'wp_hook_expose', 'wp_hook_expose' );
 
 		// Add a settings section for the "Post Saved" event.
-		$this->register_section_event_webhook_post_saved();
+		$this->register_section_event_webhook( 'Post Saved', 'post_saved' );
 	}
 
 	/**
@@ -67,67 +67,77 @@ class Options {
 		<?php
 	}
 
-	/**
-	 * Register the settings section and fields for the "Post Saved" event.
-	 */
-	public function register_section_event_webhook_post_saved(): void {
+	public function register_section_event_webhook( string $event_title, string $event_slug ): void {
 		// Register the settings section.
 		add_settings_section(
-			'wp_hook_expose_event_webhook_post_saved',
-			__( 'Post Saved Webhook', 'wp-hook-expose' ),
-			array( $this, 'render_section_event_webhook_post_saved' ),
+			'wp_hook_expose_event_webhook_' . $event_slug,
+			$event_title . ' Webhook',
+			function () use ( $event_title, $event_slug ) {
+				$this->render_section_event_webhook( $event_title, $event_slug );
+			},
 			'wp_hook_expose'
 		);
 
 		// Register the settings field for the "Post Saved" webhook URL.
 		add_settings_field(
-			'wp_hook_expose_event_webhook_post_saved_url',
+			'wp_hook_expose_event_webhook_' . $event_slug . '_url',
 			__( 'Webhook URL', 'wp-hook-expose' ),
-			array( $this, 'render_field_event_webhook_post_saved_url' ),
+			function () use ( $event_title, $event_slug ) {
+				$this->render_field_event_webhook_url( $event_title, $event_slug );
+			},
 			'wp_hook_expose',
-			'wp_hook_expose_event_webhook_post_saved'
+			'wp_hook_expose_event_webhook_' . $event_slug
 		);
 
 		// Register the settings field for the "Post Saved" webhook body.
 		add_settings_field(
-			'wp_hook_expose_event_webhook_post_saved_body',
+			'wp_hook_expose_event_webhook_' . $event_slug . '_body',
 			__( 'Webhook Request Body', 'wp-hook-expose' ),
-			array( $this, 'render_field_event_webhook_post_saved_body' ),
+			function () use ( $event_title, $event_slug ) {
+				$this->render_field_event_webhook_body( $event_title, $event_slug );
+			},
 			'wp_hook_expose',
-			'wp_hook_expose_event_webhook_post_saved'
+			'wp_hook_expose_event_webhook_' . $event_slug
 		);
 	}
 
 	/**
 	 * Render the settings section for the "Post Saved" event.
 	 */
-	public function render_section_event_webhook_post_saved(): void {
+	public function render_section_event_webhook( string $event_title, string $event_slug ): void {
 		?>
-        <p><?php esc_html_e( 'This section allows you to map the "Post Saved" event to a URL.', 'wp-hook-expose' ); ?></p>
+        <p>
+			<?php
+			sprintf(
+				esc_html__( 'This section allows you to map the "%s" event to a URL.', 'wp-hook-expose' ),
+				$event_title
+			);
+			?>
+        </p>
         <p><?php esc_html_e( 'The URL will be registered as a webhook and POST requests will be sent to it as soon as the event occurs.', 'wp-hook-expose' ); ?></p>
 		<?php
 	}
 
 	/**
-	 * Render the settings field for the "Post Saved" webhook URL.
+	 * Render the settings field for the event webhook's URL.
 	 */
-	public function render_field_event_webhook_post_saved_url(): void {
+	public function render_field_event_webhook_url( string $event_title, string $event_slug ): void {
 		$options = get_option( 'wp_hook_expose' );
 		?>
-        <input type="text" name="wp_hook_expose[event_webhook_post_saved][url]"
-               value="<?php echo esc_attr( $options['event_webhook_post_saved']['url'] ?? '' ); ?>">
+        <input type="text" name="wp_hook_expose[event_webhooks][<?php echo esc_attr( $event_slug ); ?>][url]"
+               value="<?php echo esc_attr( $options['event_webhooks'][ $event_slug ]['url'] ?? '' ); ?>">
         <p class="description"><?php esc_html_e( 'The URL to which the POST request will be sent.', 'wp-hook-expose' ); ?></p>
 		<?php
 	}
 
 	/**
-	 * Render the settings field for the "Post Saved" webhook body.
+	 * Render the settings field for the event webhook's body.
 	 */
-	public function render_field_event_webhook_post_saved_body(): void {
+	public function render_field_event_webhook_body( string $event_title, string $event_slug ): void {
 		$options = get_option( 'wp_hook_expose' );
 		?>
-        <textarea name="wp_hook_expose[event_webhook_post_saved][body]" rows="10"
-                  cols="50"><?php echo esc_textarea( $options['event_webhook_post_saved']['body'] ?? '' ); ?></textarea>
+        <textarea name="wp_hook_expose[event_webhooks][<?php echo esc_attr( $event_slug ); ?>][body]" rows="10"
+                  cols="50"><?php echo esc_textarea( $options['event_webhooks'][ $event_slug ]['body'] ?? '' ); ?></textarea>
         <p class="description">
 			<?php esc_html_e( 'The JSON body of the POST request.', 'wp-hook-expose' ); ?>
             <br>
