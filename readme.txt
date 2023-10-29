06adcc343626a56c40fc515d2e7fe91b0ba1f5c4
Fabian Reinders <fabian@fabiancdng.com>
2023-10-29 14:10:04 +0100

    📝Add `readme.txt` file

---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== WP Hook Expose ===
+Contributors: fabiancdng
+Tags: webhooks, expose, hook, webhook, api, rest
+Requires at least: 6.0
+Tested up to: 6.3
+Requires PHP: 7.4
+
+A simple and fast way to turn WordPress hooks into custom webhooks.
+
+== Description ==
+
+A simple and fast way to turn WordPress hooks into custom webhooks.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
--- wp-hook-expose.php
+++ wp-hook-expose.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       WP Hook Expose
  * Plugin URI:        https://github.com/fabiancdng/wp-hook-expose
- * Description:       A simple and fast way to create webhooks for WordPress events.
+ * Description:       A simple and fast way to turn WordPress hooks into custom webhooks.
  * Version:           1.0
  * Requires at least: 6.0
  * Author:            Fabian Reinders
