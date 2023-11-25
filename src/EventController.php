247c379236506d61e075cf2bd76a1856f7ea2298
Fabian Reinders <fabian@fabiancdng.com>
2023-11-25 13:05:39 +0100

    🐛Prevent webhook from being executed twice during a request
    If a WordPress action is called multiple times during a request, don't execute the webhook callback again.
    TODO: Add option to allow for multiple webhook requests per request.

--- src/EventController.php
+++ src/EventController.php
@@ -15,6 +15,11 @@ class EventController {
 	private WebhookController $webhook_controller;
 
 	/**
+	 * The actions added by the EventController for each webhook.
+	 */
+	private array $actions;
+
+	/**
 	 * The constructor for the EventController class.
 	 *
 	 * @param WebhookController $webhook_controller WebhookController instance to manage webhooks.
@@ -32,11 +37,15 @@ class EventController {
 
 		// Loop through the webhooks and subscribe the webhook callback to the WordPress hook.
 		foreach ( $webhooks as $webhook_slug => $webhook ) {
+			// Add action to the EventController.
+			$this->actions[ $webhook_slug ] = function ( ...$args ) use ( $webhook_slug, $webhook ) {
+				$this->webhook_callback( $webhook_slug, $webhook, $args );
+			};
+
+			// Add action to WordPress.
 			add_action(
 				$webhook['event'],
-				function ( ...$args ) use ( $webhook_slug ) {
-					$this->webhook_callback( $webhook_slug, $args );
-				},
+				$this->actions[ $webhook_slug ],
 				10,
 				PHP_INT_MAX
 			);
@@ -49,10 +58,18 @@ class EventController {
 	 * Sends off the request to the specified URL of the webhook.
 	 *
 	 * @param string $webhook_slug The slug of the webhook.
+	 * @param array  $webhook      The webhook data.
 	 * @param array  $args         The arguments passed to the WordPress hook.
 	 */
-	public function webhook_callback( string $webhook_slug, array $args ): void {
+	public function webhook_callback( string $webhook_slug, array $webhook, array $args ): void {
 		// Execute the webhook.
 		$this->webhook_controller->execute_webhook( $webhook_slug, $args );
+
+		// Remove the action.
+		remove_action(
+			$webhook['event'],
+			$this->actions[ $webhook_slug ],
+			10
+		);
 	}
 }
